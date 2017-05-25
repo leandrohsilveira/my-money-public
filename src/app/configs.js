ec071923a70a9b4400c937087ecc16acee4fa369
Leandro Hinckel Silveira <leandro.hinckel@gmail.com>
2017-05-25 13:43:01 -0300

    Improved error handling

--- src/app/configs.js
+++ src/app/configs.js
@@ -5,14 +5,22 @@ export const api = path => `/api/${path}`
 export const openApi = path => `/oapi/${path}`
 
 export const handleResponseError = (e) => {
-    console.log(e.response || e)
-    const title = e.response.data.message
+    console.error(e.response || e)
+    const data = e.response.data
+    const title = data.message
     switch(e.response.status) {
         case 400:
-            Object.keys(e.response.data.errors).forEach(key => toastr.error(title, e.response.data.errors[key].message.replace('Path', 'Field')))
+            Object.keys(data.errors).forEach(key =>  toastr.error(title, data.errors[key].message.replace('Path', 'Field')))
+            break
+        case 403:
+            if(data && data.errors) {
+                Object.keys(data.errors).forEach(key => toastr.error(title, data.errors[key].message))
+            } else {
+                toastr.error('Access denied', 'Sorry, you lack of permissions to access this content')
+            }
             break
         default:
-            Object.keys(e.response.data.errors).forEach(key => toastr.error(title, e.response.data.errors[key].message))
+            Object.keys(data.errors).forEach(key => toastr.error(title, data.errors[key].message))
             break
     }
 }
